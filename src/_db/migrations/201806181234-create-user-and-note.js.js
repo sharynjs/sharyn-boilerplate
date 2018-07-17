b0a784c943fa77d7a920b57c1ff0a4bbc3b693d9
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-17 09:10:29 +0300

    Babel 7 features, db migrations and seeds

---
+++ src/_db/migrations/201806181234-create-user-and-note.js.js
@@ -0,0 +1,20 @@
+const { standardCols, userIdCol } = require('@sharyn/db')
+
+module.exports = {
+  up: async knex => {
+    await knex.schema.createTable('User', t => {
+      standardCols(knex, t)
+      t.string('username')
+        .unique()
+        .notNullable()
+      t.string('passwordHash').notNullable()
+    })
+    await knex.schema.createTable('Note', t => {
+      standardCols(knex, t)
+      userIdCol(t)
+      t.string('title').notNullable()
+      t.text('description')
+    })
+  },
+  down: () => {},
+}
