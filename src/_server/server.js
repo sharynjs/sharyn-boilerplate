f68d07d3dbbefce04cdb91442f7ed391392c5a7a
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-19 11:04:49 +0300

    Add puppeteer E2E test

--- src/_server/server.js
+++ src/_server/server.js
@@ -1,10 +1,10 @@
 // @flow
 
-import { NODE_ENV } from '@sharyn/env'
+import { NODE_ENV, TESTING_PORT } from '@sharyn/env'
 import { startServer, stopServer } from '@sharyn/koa'
 import routing from '_server/routing'
 
 NODE_ENV === 'test' || startServer(routing)
 
-export const startTestServer = () => startServer(routing, { silent: true })
-export const stopTestServer = () => stopServer({ silent: true })
+export const startTestServer = () => startServer(routing, { port: TESTING_PORT, silent: true })
+export const stopTestServer = () => stopServer({ port: TESTING_PORT, silent: true })
--- src/_testing/e2e.test.js
+++ src/_testing/e2e.test.js
@@ -1,13 +1,23 @@
 // @flow
 
+import { TESTING_PORT } from '@sharyn/env'
 import { startTestServer, stopTestServer } from '_server/server'
+import puppeteer from 'puppeteer'
+import expect from 'expect-puppeteer'
 
-beforeAll(() => startTestServer())
+let browser
+
+beforeAll(async () => {
+  browser = await puppeteer.launch()
+  startTestServer()
+})
+
+afterAll(() => stopTestServer() && browser.close())
 
 beforeEach(() => console.log('reset DB'))
 
-afterAll(() => stopTestServer())
-
 test('E2E', async () => {
-  expect(true).toBe(true)
+  const page = await browser.newPage()
+  await page.goto(`http://localhost:${TESTING_PORT}`)
+  await expect(page).toMatch('it works')
 })
