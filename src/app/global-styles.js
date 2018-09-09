0983b76d2f333264a8412cf71bcbd88ab355f497
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-09 16:32:09 +0900

    Reformat global styles

--- src/app/global-styles.js
+++ src/app/global-styles.js
@@ -8,22 +8,10 @@ const globalStyles = ({ palette }: Object) => ({
       fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
       boxSizing: 'border-box',
     },
-    '*, *:before, *:after': {
-      boxSizing: 'inherit',
-    },
-    body: {
-      height: '100%',
-      margin: 0,
-    },
-    '#app': {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    a: {
-      color: palette.primary.main,
-      textDecoration: 'none',
-    },
+    '*, *:before, *:after': { boxSizing: 'inherit' },
+    body: { height: '100%', margin: 0 },
+    '#app': { height: '100%', display: 'flex', flexDirection: 'column' },
+    a: { color: palette.primary.main, textDecoration: 'none' },
   },
 })
 
